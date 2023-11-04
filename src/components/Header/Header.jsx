316ef44ad3077c1300e83ff6ca5ba150098faeb3
Loris Ducamps <lducamps@corp.free.fr>
2023-11-04 10:22:56 +0100

    update style header ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,43 +2,45 @@ import { useState } from "react"
 
 export default function Header() {
 
+    const before = "before:content-['*'] before:absolute before:top-0 before:left-0 before:right-0 before:h-[128px] before:background-linear"
+
     const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <header className="relative flex justify-between items-center max-w-[1440px] mx-auto">
+        <header className={`${before} relative flex justify-between items-center max-w-[1440px] mx-auto sm:before:hidden`}>
 
             {isOpen && <div className="fixed top-0 left-0 right-0 bottom-0 h-full background-linear z-10 sm:hidden"></div>}
 
             <picture>
                 <source media="(min-width: 376px)" srcSet="/assets/img/image-hero-desktop.jpg 1x, /assets/img/image-hero-desktop_2x.jpg 2x" type="image/jpg" />
                 <source media="(max-width: 375px)" srcSet="/assets/img/image-hero-mobile.jpg 1x, /assets/img/image-hero-mobile_2x.jpg 2x" type="image/jpg" />
-                <img src="/assets/img/image-hero-desktop.jpg" type="image/jpg" />
+                <img src="/assets/img/image-hero-desktop.jpg" type="image/jpg" alt="Bannière bureau de travail" />
             </picture>
 
-            <nav className="absolute top-8 left-0 right-0 max-w-[1110px] mx-auto w-full z-20 sm:top-12">
-                <div className="flex justify-between px-6">
-                    <a href="#" className="block">
+            <nav className="absolute top-4 left-0 right-0 max-w-[1110px] mx-auto w-full z-20 sm:top-12">
+                <div className="flex justify-between items-center px-6">
+                    <a href="#" className="block py-4 sm:p-0">
                         <img src="/assets/img/logo.svg" alt="Logo CrowdFund" />
                     </a>
                     <ul className="hidden sm:flex sm:gap-8">
                         <li>
-                            <a className="text-white text-[13px] font-medium" href="#">About</a>
+                            <a className="block text-white text-[13px] font-medium hover:underline" href="#">About</a>
                         </li>
                         <li>
-                            <a className="text-white text-[13px] font-medium" href="#">Discover</a>
+                            <a className="block text-white text-[13px] font-medium hover:underline" href="#">Discover</a>
                         </li>
                         <li>
-                            <a className="text-white text-[13px] font-medium" href="#">Get Started</a>
+                            <a className="block text-white text-[13px] font-medium hover:underline" href="#">Get Started</a>
                         </li>
                     </ul>
-                    <img onClick={() => setIsOpen(!isOpen)} className="z-10 cursor-pointer sm:hidden" src={isOpen ? "/assets/img/close.svg" : "/assets/img/menu.svg"} alt="icon" />
+                    <img onClick={() => setIsOpen(!isOpen)} className="pt-4 pl-4 pb-4 z-10 cursor-pointer sm:hidden" src={isOpen ? "/assets/img/close.svg" : "/assets/img/menu.svg"} alt="icon" />
                 </div>
             </nav>
 
 
             {isOpen && (
-                <nav className="absolute top-[88px] left-0 right-0 px-6 z-20 sm:hidden">
-                    <ul className="border border-custom rounded-lg divide-y divide-dark/10 bg-white">
+                <nav className="absolute top-[88px] right-0 left-0 px-6 z-20 sm:hidden">
+                    <ul className="border border-light-black rounded-lg divide-y divide-dark/10 bg-white">
                         <li>
                             <a className="block text-lg p-6" href="#">About</a>
                         </li>
